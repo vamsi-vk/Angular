472d881916b48aae44e71dd18bc37be423ccb17a
vamsi-vk <vamsikrishnaaketi@gmail.com>
2023-08-17 11:29:00 +0530

    first angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseComponent,
+    CourseComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/course.component.ts
@@ -0,0 +1,15 @@
+
+import {Component} from '@angular/core'
+
+@Component(
+    {
+        selector : "cources",
+        template : "<h1> Hello WOrld This is vamsi <h1>"
+        
+    }
+)
+export class CourseComponent {
+
+    
+
+}
